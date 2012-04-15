7a550ce5d67d30de943e4d92ccb1cb82a11e2543
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2012-04-15 18:35:49 +0200

    Selection will be parsed and if HEX color is selected it will be fully replaced with picked color.

--- colorsnapper_command.py
+++ colorsnapper_command.py
@@ -11,6 +11,27 @@ class PickWithColorSnapperCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         view = self.view
+
+        # Pick a color
+        color = self.run_color_picker()
+
+        # For each selected region
+        for region in view.sel():
+            # get a word from region
+            word = view.word(region)
+            # see if it's a color in HEX format
+            if self.is_valid_hex_color(view.substr(word)):
+                # and if it starts with #
+                if view.substr(word.a - 1) == '#':
+                    # increase selection to include '#'
+                    word = sublime.Region(word.a - 1, word.b)
+                    # and replace it with picked color
+                    view.replace(edit, word, str(color))
+            else:
+                # otherwise just replace selection
+                view.replace(edit, region, str(color))
+
+    def run_color_picker(self):
         args = "~/Downloads/ColorSnapper.app/Contents/MacOS/ColorSnapper cli --format cssHEXUpper -m 2"
         try:
             process = subprocess.Popen( args,
@@ -22,10 +43,15 @@ class PickWithColorSnapperCommand(sublime_plugin.TextCommand):
             if error != '':
                 sublime.errorMessage(error.decode('utf-8'))
             else:
-                for region in view.sel():
-                    view.replace(edit, region, str(result.decode('utf-8')))
+                return result.decode('utf-8')
 
         except OSError, error:
             sublime.errorMessage(error.decode('utf-8'))
 
-        edit.end_edit()
+    def is_valid_hex_color(self, s):
+      if len(s) not in (3, 6):
+        return False
+      try:
+        return 0 <= int(s, 16) <= 0xffffff
+      except ValueError:
+        return False
